5806de21722a11db7822a60d2cd140a8eff6ab86
Kyle Choi <choikyle96@gmail.com>
2020-08-24 16:22:43 -0400

    reorganized structure, added tests, changed some regex

--- src/docScan.py
+++ src/docScan.py
@@ -2,8 +2,10 @@ import re
 import fake as f
 
 def docToText(filename):
-    file = open(filename, "r")
-    return file.read()
+    # use with/as to close file resource after using it
+    with open(filename, "r") as f:
+        file = f.read()
+        return file
 
 def snapToText():
     # screenshot of text
@@ -20,8 +22,11 @@ def textToAnswers(txt):
     d = dict((num, answers[num-1]) for num in nums)
     return d
 
+# need to account for decimal and fraction answers. split() should not split up those answers
 def numberedTextToAnswers(txt):
     txt = txt.upper()
+    # using python lookarounds
+    txt = re.sub("(?<=[0-9])(\\.)(?=[0-9])", "+", txt)
     txt = re.split("[ \\.\\)\\-\\n]+", txt)
     d = dict(zip(txt[::2], txt[1::2]))
     return d
@@ -53,4 +58,4 @@ if __name__ == "__main__":
     count = grader(marked, real)
     createTextFile(count)
 
-    f.test()
+    # f.test()
---
+++ testing/TextFileTests.py
@@ -0,0 +1,44 @@
+import unittest
+import sys
+# Is there another way to import from different folder without using folder path?
+sys.path.insert(1, "C:/Users/Kyle/Desktop/CODE/Python/AutoGrader/src")
+from docScan import docToText, textToAnswers, numberedTextToAnswers, grader
+
+class TextTestCase(unittest.TestCase):
+    # every text file should be parsed to be the same as 'answers'
+    # all dots in answers should've been converted to +
+    answers = {"1": "A", "2": "B", "3": "C", "4": "D", "5": "A", "6": "B", "7": "12", "8": "0+25", "9": "9/4", "10": "3+14"}
+
+    # need to start with 'test'!!
+    def test_dot(self):
+        file = docToText("testfiles/neatDot.txt")
+        read = numberedTextToAnswers(file)
+        self.assertEqual(read, TextTestCase.answers)
+    
+    def test_space(self):
+        file = docToText("testfiles/neatSpace.txt")
+        read = numberedTextToAnswers(file)
+        self.assertEqual(read, TextTestCase.answers)
+    
+    def test_dash(self):
+        file = docToText("testfiles/neatDash.txt")
+        read = numberedTextToAnswers(file)
+        self.assertEqual(read, TextTestCase.answers)
+    
+    def test_parenthesis(self):
+        file = docToText("testfiles/neatParen.txt")
+        read = numberedTextToAnswers(file)
+        self.assertEqual(read, TextTestCase.answers)
+
+    def test_mixed(self):
+        file = docToText("testfiles/neatMixed.txt")
+        read = numberedTextToAnswers(file)
+        self.assertEqual(read, TextTestCase.answers)        
+
+
+
+
+
+if __name__=="__main__":
+    unittest.main()
+
